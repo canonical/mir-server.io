bdf3d3b07b28403088792ac01f9323d72c7820d2
Pete <peterfrench94@gmail.com>
2024-09-19 09:21:25 +0200

    chore: Upgrade canonicalweb.search to 2.1.0 & canoicalwebteam.flask-base 2.0.0

--- webapp/app.py
+++ webapp/app.py
@@ -66,9 +66,11 @@ app.add_url_rule(
     "/docs/search",
     "docs-search",
     build_search_view(
+        app=app,
         session=session,
         site="mir-server.io",
         template_path="docs/search.html",
+        request_limit="1/day",
     ),
 )
 
